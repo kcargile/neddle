f6214056c73eb9f9fd18ceff7ad2f152aa3bedbf
Kristopher Cargile <kris@kriscargile.com>
2012-08-03 11:07:36 -0400

    initial core project setup

---
+++ neddle/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("neddle.core")]
+[assembly: AssemblyDescription("Neddle LMS Core Library")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3c95ae7d-881a-428e-97d0-a4fb4e2b7360")]
