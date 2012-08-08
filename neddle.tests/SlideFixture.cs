8f720003426ad21c2355220433a1546d7b89c86d
Kristopher Cargile <kris@kriscargile.com>
2012-08-08 14:49:38 -0400

    add slide entity

---
+++ neddle.tests/SlideFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Neddle.Tests
+{
+    [TestFixture]
+    public class SlideFixture : NeddleObjectFixture<Slide>
+    {
+        [Test]
+        public override void Equals()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- neddle/Chapter.cs
+++ neddle/Chapter.cs
@@ -1,9 +1,67 @@
-﻿namespace Neddle
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace Neddle
 {
     /// <summary>
     /// A section within a <see cref="Course"/>.
     /// </summary>
     public class Chapter : NeddleObject<Chapter>
     {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        [DataMember]
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slides.
+        /// </summary>
+        /// <value>
+        /// The slides.
+        /// </value>
+        [DataMember]
+        [XmlArray(ElementName = "slides")]
+        [XmlArrayItem(ElementName = "slide", Type = typeof(Slide))]
+        public List<Slide> Slides { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Neddle.Slide"/> at the specified index.
+        /// </summary>
+        [XmlIgnore]
+        public Slide this[int index]
+        {
+            get
+            {
+                if (null != Slides && Slides.Count >= index)
+                {
+                    return Slides[index];
+                }
+
+                return null;
+            }
+            set
+            {
+                if (null == Slides)
+                {
+                    Slides = new List<Slide>();
+                }
+
+                Slides[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Chapter"/> class.
+        /// </summary>
+        public Chapter()
+        {
+            Slides = new List<Slide>();
+        }
     }
 }
